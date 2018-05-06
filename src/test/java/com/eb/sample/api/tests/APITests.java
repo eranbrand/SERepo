e059a7aecc984870a28d49394e911ede154317af
Eran Brand <eranbrand@Erans-MacBook-Pro.local>
2018-05-06 17:14:03 +0300

    Get random jokes and jokes by id

--- src/test/java/com/eb/sample/api/tests/APITests.java
+++ src/test/java/com/eb/sample/api/tests/APITests.java
@@ -1,5 +1,22 @@
 package com.eb.sample.api.tests;
 
-public class APITests {
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
+public class APITests {
+    public static void main(String[] args) throws UnirestException {
+        String randJokeURL = "https://icanhazdadjoke.com";
+        String jokeByIdURL = "https://icanhazdadjoke.com/j/";
+        final HttpResponse<JsonNode> response01 = Unirest.get(randJokeURL).header("accept", "application/json").asJson();
+        String joke = response01.getBody().getObject().getString("joke");
+        System.out.println("\n\n>>>>>>>          " + joke);
+
+
+        final HttpResponse<JsonNode> response02 = Unirest.get(jokeByIdURL + "R7UfaahVfFd").header("accept", "application/json").asJson();
+        String joke2 = response02.getBody().getObject().getString("joke");
+        System.out.println("\n\n>>>>>>>          " + joke2);
+        System.out.println("Very funny!");
+    }
 }
